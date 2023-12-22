4799adc45e8809b00f456d95423ffcd3cc8c01ee
Raphael Lesacher <80222395+raphilesacher@users.noreply.github.com>
2023-12-22 17:06:48 +0100

    refactored Services in order to applied Dependency Inversion Principle by adding a layer of abstraction

--- src/main/java/com/example/Marketplace/service/TokenService.java
+++ src/main/java/com/example/Marketplace/service/TokenService.java
@@ -2,7 +2,9 @@ package com.example.Marketplace.service;
 
 import com.example.Marketplace.model.User;
 
+
 public interface TokenService {
     String generateToken(User user);
     boolean validateToken(String token, User user);
+
 }
