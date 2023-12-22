804d36c08ce98e8182c197c535b48864a2499ab8
Raphael Lesacher <80222395+raphilesacher@users.noreply.github.com>
2023-12-22 16:50:47 +0100

    refactored Services in order to applied Dependency Inversion Principle by adding a layer of abstraction

--- src/main/java/com/example/Marketplace/service/UserService.java
+++ src/main/java/com/example/Marketplace/service/UserService.java
@@ -1,62 +1,18 @@
 package com.example.Marketplace.service;
 
 import com.example.Marketplace.model.User;
-import com.example.Marketplace.repository.UserRepository;
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
+
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Service;
 
-/**
- * This class is used to hold the functionality for the LoginController
- */
-@Service
-public class UserService {
-    @Autowired
-    UserRepository userRepository;
-
-    @Autowired
-    private PasswordEncoder passwordEncoder;
-
-    @Autowired
-    private TokenService tokenService;
-
-
-    public UserService(){}
+public interface UserService {
 
     /**
      * This method is used to perform the login authentication
      * @param user the user to be logged in
      * @return true if login success, return false if login failed
      */
-    public boolean loginUser(User user) {
-        System.out.println("Username check: " + user.getUsername());
-        System.out.println("Pw check: " + user.getPw());
-
-        if (userRepository.checkUserExists(user.getUsername())) { // if user exists in db
-
-            System.out.println("USER EXISTS");
-            // retrieve user from userRepo
-            User loginUser = userRepository.findByUserName(user.getUsername());
-
-            // authenticate user
-            if (passwordEncoder.matches(user.getPw(), loginUser.getPw())) { // pw correct
-                System.out.println("PW Correct");
-                return true;
-
-            } else { // pw incorrect
-                System.out.println("PW Incorrect");
-                return false;
-            }
-
-        }else{ // user not existing
-                System.out.println("USER NOT EXISTING");
-                return false;
-            }
-    }
+    boolean loginUser(User user);
 
     /**
      * This method generates the JWT token for the user and stores it in a cookie and returns the response
@@ -65,49 +21,12 @@ public class UserService {
      * @param responseBody the string that should be added to the response body
      * @return the ResponseEntity after token is created and stored in cookie.
      */
-    public ResponseEntity<String> createTokenAndCookie(User loginUser, HttpServletResponse response, String responseBody) {
-
-
-        String userToken = tokenService.generateToken(loginUser);
-        // create http header and add token
-        HttpHeaders header = new HttpHeaders();
-        header.add("Authorization", "Bearer " + userToken);
-        // Include the Location header for redirection
-        response.addHeader("Location", "/" + responseBody);
-        // Add the token to the response
-        //model.addAttribute(userToken);
-
-        // Set cookie
-        Cookie cookie = new Cookie("token", userToken);
-        cookie.setPath("/");
-        cookie.setHttpOnly(true);
-        response.addCookie(cookie);
-
-        // TODO: if input correct route to homepage
-        System.out.println(userToken);
-
-        return ResponseEntity.ok().headers(header).body(responseBody);
-    }
+    ResponseEntity<String> createTokenAndCookie(User loginUser, HttpServletResponse response, String responseBody);
 
     /**
      * This method is used to perform the registration authentication
      * @param user the user to be logged in
      * @return true if registration success, return false if registration failed
      */
-    public boolean registerUser(User user) {
-        if (!userRepository.checkUserExists(user.getUsername())) {
-            String encodedPassword = passwordEncoder.encode(user.getPw());
-            System.out.println("Insert user " + user.getUsername() + " with pw " + user.getPw());
-            user.setPw(encodedPassword);
-            userRepository.saveAndFlush(user);
-
-            // user registered return true
-            return true;
-
-        } else {
-            // user already exists
-            System.out.println("User already exists");
-            return false;
-        }
-    }
+    boolean registerUser(User user);
 }
---
+++ src/main/java/com/example/Marketplace/service/impl/TokenServiceImpl.java
@@ -0,0 +1,63 @@
+package com.example.Marketplace.service.impl;
+
+import com.example.Marketplace.model.User;
+
+import com.example.Marketplace.service.TokenService;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+
+@Service
+public class TokenServiceImpl implements TokenService {
+    private static final Key SECRET_KEY = Keys.secretKeyFor(SignatureAlgorithm.HS256);;
+    @Override
+    public String generateToken(User user) {
+        Map<String, Object> claims = new HashMap<>();
+        // print key just for verification purpose on jwt.io
+        System.out.println("JWT KEY: " + io.jsonwebtoken.io.Encoders.BASE64.encode(SECRET_KEY.getEncoded()));
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(user.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000*3600*12)) //set expiration time of token to 12 hours
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .compact();
+    }
+
+    @Override
+    public boolean validateToken(String token, User user) {
+        //extract username from token
+        String username = getUsernameFromToken(token);
+        return (username.equals(user.getUsername()) && !isExpired(token));
+    }
+
+    private String getUsernameFromToken(String token) {
+        // parse subject from token
+        return Jwts.parser()
+                .setSigningKey(SECRET_KEY)
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    private boolean isExpired(String token) {
+        // parse expiration before token
+        Date expiration = Jwts.parser()
+                            .setSigningKey(SECRET_KEY)
+                            .parseClaimsJws(token)
+                            .getBody()
+                            .getExpiration();
+
+        return expiration.before(new Date());
+    }
+
+
+
+}
